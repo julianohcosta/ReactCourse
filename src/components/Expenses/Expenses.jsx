37a455436f52786c779b43297d59955acd168a4b
Juliano Hausen Oliveira da Costa <juliano.h.costa@rfb.gov.br>
2022-07-21 17:50:38 -0400

    task 2

---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,30 @@
+import './css/Expenses.css'
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "../UI/ExpensesFilter";
+
+/**
+ *
+ * @param {Object} props object with the following attributes: title {String}, amount {Number} and date {Date}
+ * @returns {JSX.Element} The JSX element
+ * @constructor
+ */
+export default function Expense(props){
+
+    const expenses = props.expenses;
+
+    const expensesFilterHandler = (year) => {
+        console.log(year)
+    }
+
+    return (
+        <div>
+            <ExpensesFilter onFilterChange={expensesFilterHandler}/>
+            <Card className="expenses">
+                <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+                <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+                <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+                <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+            </Card>
+        </div>
+)}
