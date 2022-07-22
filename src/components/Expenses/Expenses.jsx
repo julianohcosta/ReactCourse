ed3a95ee8b2a09339acd4f9ca486c9a5aec46273
Juliano Costa <julianohcosta@gmail.com>
2022-07-21 21:31:15 -0400

    Add filter logic

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,5 +1,5 @@
 import './css/Expenses.css'
-import { useState } from 'react';
+import {useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "../UI/ExpensesFilter";
@@ -13,20 +13,27 @@ import ExpensesFilter from "../UI/ExpensesFilter";
 export default function Expense(props) {
 
     const [filteredYear, setFilteredYear] = useState('2020');
-    const expenses = props.expenses;
 
     const expensesFilterHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter onFilterChange={expensesFilterHandler} selectedYear={filteredYear}/>
-                <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-                <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-                <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-                <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+                {
+                    filteredExpenses.length === 0 ? <p>No Expenses found.</p> : filteredExpenses.map((expense) => (
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}/>
+                    ))}
             </Card>
         </div>
     )
